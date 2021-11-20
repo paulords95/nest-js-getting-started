2fcfe1de36463ec8298a56cb81d1f3f1256b9f0e
Paulo <paulords95@gmail.com>
2021-11-20 14:21:04 -0300

    nest.js test project

---
+++ src/Controllers/products/products.controller.ts
@@ -0,0 +1,42 @@
+import { Controller, Post, Body, Get, Param, Patch } from '@nestjs/common';
+import { ProductsService } from './products.service';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  addProduct(
+    @Body('title') prodTtile: string,
+    @Body('description') prodDesc: string,
+    @Body('price') prodPrice: number,
+  ) {
+    const generatedId = this.productsService.insertProduct(
+      prodTtile,
+      prodDesc,
+      prodPrice,
+    );
+    return { id: generatedId };
+  }
+
+  @Get()
+  getAllProducts() {
+    return this.productsService.getProducts();
+  }
+
+  @Get(':id')
+  getProduct(@Param('id') prodId: string) {
+    return this.productsService.getSingleProduct(prodId);
+  }
+
+  @Patch(':id')
+  updateProduct(
+    @Param('id') prodId: string,
+    @Body('title') title: string,
+    @Body('description') description: string,
+    @Body('price') price: number,
+  ) {
+    this.productsService.updateProduct(prodId, title, description, price);
+    return null;
+  }
+}
